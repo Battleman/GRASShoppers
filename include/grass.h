fa6105de0d45843c57f702b24e54f8834d9ea74f
Aymeric Genêt <aymeric.gen@gmail.com>
2019-04-23 13:23:49 +0200

    Add GRASS miscellaneous functions.

--- include/grass.h
+++ include/grass.h
@@ -1,3 +1,10 @@
+/*
+ * grass.h : GRASS miscellaneous header
+ *
+ *  Created on: Apr 22, 2019
+ *      Author: Aymeric Genêt
+ */
+
 #ifndef GRASS_H
 #define GRASS_H
 
@@ -16,6 +23,10 @@
 #include <stdbool.h>
 #include <sys/stat.h>
 
+
+
+#define TOKENS_DELIM " \t\r\n" 
+
 struct User {
     const char* uname;
     const char* pass;
@@ -31,4 +42,35 @@ struct Command {
 
 void hijack_flow();
 
-#endif
+/*
+ * Parses received line into a specified number of tokens.
+ *
+ * @parameter cmd
+ *   Placeholder array pre-initialized with n_tok elements that will contain
+ *   the parsed command and arguments. The last element will always be NULL.
+ * @parameter line
+ *   Line to parse, must be NULL-terminated.
+ * @parameter n_tok
+ *   Maximum number of tokens.
+ */
+
+void parse_line(char** cmd, char* line, int n_tok);
+
+/*
+ * Executes parsed line and transfers its output and return status.
+ *
+ * @parameter output
+ *   Placeholder array pre-initialized with output_len elements that will
+ *   contain the output of executed command.
+ * @parameter output_len
+ *   Available length of output string.
+ * @parameter cmd
+ *   Command and arguments to execute.
+ *
+ * @return
+ *   Return status of the command executed.
+ */
+
+int launch(char *output, int output_len, char **cmd);
+
+#endif /* GRASS_H */
--- src/grass.c
+++ src/grass.c
@@ -1,6 +1,74 @@
-#include <grass.h>
+#include "grass.h"
+
 #include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
 
 void hijack_flow(){
-	printf("Method hijack: Accepted\n");
+    printf("Method hijack: Accepted\n");
+}
+
+void parse_line(char** cmd, char* line, int n_tok) {
+    int idx = 0;
+    char *token = NULL;
+
+    token = strtok(line, TOKENS_DELIM);
+    while (token != NULL && idx < (n_tok - 1)) {
+        cmd[idx++] = token;
+        token = strtok(NULL, TOKENS_DELIM);
+    }
+
+    cmd[idx] = NULL;
+}
+
+int launch(char *output, int output_len, char **cmd) {
+    pid_t pid = 0;
+    int status = 0;
+    int comm[2];
+
+    /* Creates a pipe between forked proceses */
+    if (pipe(comm) == -1) {
+        perror("Pipe failed");
+        return -1; /* Error */
+    }
+
+    /* Forks the command execution */
+    pid = fork();
+
+    if (pid == 0) {
+        /* Child process */
+
+        /* Redirects stdout to communication link */
+        dup2(comm[1], STDOUT_FILENO);
+
+        close(comm[0]);
+        close(comm[1]);
+
+        execvp(cmd[0], cmd);
+
+        perror("Execution failed");
+        exit(EXIT_FAILURE); /* Forces child to die */
+    } else if (pid < 0) {
+        /* Error forking */
+        perror("Fork failed");
+        return -1; /* Error */
+    } else {
+        /* Parent process */
+
+        /* Waits for child to die (#novaxx) */
+        waitpid(pid, &status, 0);
+
+        /* Reads communication from child process on success */
+        if (status == 0) {
+            read(comm[0], output, output_len);
+        }
+        output[output_len - 1] = '\0';
+
+        close(comm[0]);
+        close(comm[1]);
+    }
+
+    return status;
 }
